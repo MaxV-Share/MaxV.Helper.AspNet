cc1ab08049114938541b07862b256690dfac6590
Max-VinhLTT <thevinh.it.kh.1997@outlook.com>
2021-04-29 22:10:20 +0700

    first commit

---
+++ MaxV.Helper/Entities/GlobalVariable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaxV.Helper.Entities
+{
+    public class GlobalVariable<T>
+    {
+        public GlobalVariable(T Object)
+        {
+            _object = Object;
+        }
+
+        private static T _object;
+        public static T Object
+        {
+            get => _object;
+        }
+    }
+}
---
+++ MaxV.Helper/ParserHelper.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaxV.Helper
+{
+    public static class ParserHelper
+    {
+        /// <summary>
+        /// Parse a object to string of json structure
+        /// </summary>
+        public static string TryParseToString<T>(T data)
+        {
+            string result = string.Empty;
+            try
+            {
+                result = JsonConvert.SerializeObject(data);
+                return result;
+            }
+            catch
+            {
+                return result;
+            }
+        }
+    }
+}
